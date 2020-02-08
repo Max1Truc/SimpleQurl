f983048c4b8246b5b00e6d4bce7b802922d3ddb7
Max1Truc <maxitruc.bonbec@gmail.com>
2020-02-08 13:25:07 +0100

    Update url shortener service (Google URL Shortener to 0x0.st)

--- popup.js
+++ popup.js
@@ -1,4 +1,4 @@
-function getCurrentTabUrl (callback) {
+function getCurrentTabUrl(callback) {
   var queryInfo = {
     active: true,
     currentWindow: true
@@ -11,27 +11,37 @@ function getCurrentTabUrl (callback) {
   });
 }
 
+function shortenURL(url, callback) {
+  if (url.length > 150) { // If the length of the URL is greater than 150 chars
+    // It uses the Framalink URL Shortener
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) { // Connected
+        if (this.responseText.length < 100) {
+          callback(this.responseText);
+        } else {
+          callback(url);
+        }
+      }
+      if (this.readyState == 4 && this.status == 0) { // No Internet Connection
+        console.log("No internet !")
+        callback(url);
+      }
+      if (this.readyState == 4) console.log("Answer : " + this.responseText);
+    };
+    xhttp.open("POST", "http://0x0.st", true);
+    xhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+    xhttp.send("shorten=" + encodeURIComponent(url));
+  } else { // URL is short
+    new QRCode(document.getElementById("qrcode"), url);
+  }
+}
+
 document.addEventListener('DOMContentLoaded', () => {
   // Get the URL of the Current tab the call the "callback"
   getCurrentTabUrl((tabUrl) => {
-
-    if (tabUrl.length > 150) { // If the length of the URL is greater than 150 chars
-      // It uses the Framalink URL Shortener
-      var xhttp = new XMLHttpRequest();
-      xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {// Connected
-          new QRCode(document.getElementById("qrcode"), JSON.parse(this.responseText).short);
-        }
-        if (this.readyState == 4 && this.status == 0) {// No Internet Connection
-          new QRCode(document.getElementById("qrcode"), tabUrl);
-        }
-        console.log(this.responseText);
-      };
-      xhttp.open("POST", "https://frama.link/a", true);
-      xhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-      xhttp.send("lsturl="+encodeURI(tabUrl)+"&format=json");
-    } else { // URL is short
-      new QRCode(document.getElementById("qrcode"), tabUrl);
-    }
+    shortenURL(tabUrl, (url) => {
+      new QRCode(document.getElementById("qrcode"), url)
+    })
   });
 });
